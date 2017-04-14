e84669acf0fbb3b25c595e7e7f5c2bea5eacf3e6
Nataly Alvarez <NatalyAlvarez@gmail.com>
2017-04-14 01:18:34 -0400

    2:Crear una extension buscar

--- js/extension.js
+++ js/extension.js
@@ -3,3 +3,17 @@ console.log( animal );
 console.log( 'el elemento que se eliminara es perro y queda :' );
 animal = animal.filter(function(i) { return i !== 'perro' }); // filtramos
 console.log( animal );
+
+var animal = ['gato', 'perro', 'gato','gallina','gallina'];
+console.log(animal);
+Array.prototype.buscar = function(animal){
+	var mensaje = 'El animal no ha sido encontrado';
+	this.forEach(function(elemento, index){
+		if (elemento == animal) {
+			mensaje = 'El animal :'+animal+' ha sido encontrado en la posicion '+index+' del arreglo';
+		}
+	});
+	return mensaje;
+}
+
+console.log(animal.buscar('perro'));
